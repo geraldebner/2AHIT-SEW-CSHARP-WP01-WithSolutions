fd904999a301afbcb5948ebca15f9304e77e2ff9
Gerald <ebnergerald@hotmail.com>
2023-06-26 16:13:23 +0200

    f

--- 02-leap-year/02-leap-year.cs
+++ 02-leap-year/02-leap-year.cs
@@ -9,7 +9,7 @@
                 if (year % 100 == 0)
                 {
                     if (year % 400 == 0)
-                        retrn true;
+                        return true;
                     else
                         return false;
                 }
